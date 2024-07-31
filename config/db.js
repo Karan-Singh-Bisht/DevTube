659f74c118c8541f899779507918ceb12991f429
Karan <karansbisht7@gmail.com>
2024-07-31 21:50:11 +0530

    Debugged DB error

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ const { DB_NAME } = require("../constant");
 
 const db = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
-      useFindAndModify: false,
-      useCreateIndex: true,
-    });
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     console.log("DB CONNECTED");
   } catch (err) {
     console.log("Couldn't connect to DB", err);
